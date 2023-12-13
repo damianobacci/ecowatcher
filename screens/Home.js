9c9e2d068cf3b67d26636c13d3acbce89f51fb4c
Damiano Bacci <damiano.bacci@gmail.com>
2023-12-13 12:40:50 +0100

    Home styling

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import OutlinedButton from "../components/ui/OutlinedButton";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Home() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/logo.png")}
+        />
+      </View>
+      <Text>Ecowatcher</Text>
       <Text>Open up App to start working on your app!</Text>
       <StatusBar style="dark" />
       <OutlinedButton icon="camera" onPress={() => alert("asd")}>
@@ -20,5 +28,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    width: 250,
+    height: 250,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
